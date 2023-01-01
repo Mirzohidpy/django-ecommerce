cd69e7d7c090d3a966405f38c46f163c54348b3c
Mirzohidpy <103629353+Mirzohidpy@users.noreply.github.com>
2023-01-02 04:52:42 +0500

    fucked up

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -137,6 +137,12 @@ from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    messages.SUCCESS: 'alert-success',
+    # messages.SUCCESS: 'alert-success',
     # messages.INFO: 'alert-info',
 }
+
+# Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'usefullpy@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwerty'
--- members/views.py
+++ members/views.py
@@ -1,6 +1,12 @@
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from members.forms import RegisterForm
 from members.models import Member
@@ -27,8 +33,22 @@ def register(request):
                 username=username,
             )
             user.save()
-            messages.success(request, 'Account was created for ' + username)
-            return redirect('register')
+
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('members/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            send_email.send()
+            # messages.success(request, 'Account was created for ' + username)
+            return redirect('members/login/?command=verification&email=' + email)
     else:
         form = RegisterForm()
     context = {
@@ -60,3 +80,23 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are now logged out')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Member._default_manager.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, Member.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Account activated successfully')
+        # auth.login(request, user)
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
