63a141988c498875708a8cad115f82a71dbed4af
Mirzohidpy <103629353+Mirzohidpy@users.noreply.github.com>
2023-01-06 03:07:01 +0500

    email settings updated

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -158,8 +158,8 @@ GRAPH_MODELS = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nefritkibera@gmail.com'
-EMAIL_HOST_PASSWORD = 'vrtpejwookqnimug'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
